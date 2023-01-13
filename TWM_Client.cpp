c29aff596cee67d3d58c37dbbc4e10796696f502
M.Porubsky <porubsky1995@hotmail.com>
2023-01-14 00:16:44 +0100

    Password hidden, all lengths

--- TWM_Client.cpp
+++ TWM_Client.cpp
@@ -50,46 +50,6 @@ void sendUser(int create_socket, char* buffer){
    send(create_socket, buffer, size, 0);
 }
 
-
-int getch(){
-   int ch;
-   struct termios t_old, t_new;
-   tcgetattr(STDIN_FILENO, &t_old);
-   t_new = t_old;
-   t_new.c_lflag &= ~(ICANON | ECHO);
-   tcsetattr(STDIN_FILENO, TCSANOW, &t_new);
-   ch = getchar();
-   tcsetattr(STDIN_FILENO, TCSANOW, &t_old);
-   return ch;
-}
-
-const char *getpass(){
-   int show_asterisk = 0;
-   const char BACKSPACE = 127;
-   const char RETURN = 10;
-   unsigned char ch = 0;
-   std::string password;
-   printf("Password >> ");
-   while ((ch = getch()) != RETURN){
-      if (ch == BACKSPACE){
-         if (password.length() != 0){
-            if (show_asterisk){
-               printf("\b \b"); // backslash: \b
-            }
-            password.resize(password.length() - 1);
-         }
-      }
-      else{
-         password += ch;
-         if (show_asterisk){
-            printf("*");
-         }
-      }
-   }
-   printf("\n");
-   return password.c_str();
-}
-
 void recvErrorMsgHandling(int size){
    if (size == -1){
       perror("recv error");
@@ -186,8 +146,20 @@ int main(int argc, char* argv[]){
             size = strlen(buffer);
             buffer[size-1] = '\0';
             send(create_socket, buffer, size, 0);
-            strcpy(buffer, getpass());
+            //strcpy(buffer, getpass());
+            // super secret START
+            printf("LDAP Password >> ");
+            struct termios t_old, t_new;
+            tcgetattr(STDIN_FILENO, &t_old);
+            t_new = t_old;
+            t_new.c_lflag &= ~(ICANON | ECHO);
+            tcsetattr(STDIN_FILENO, TCSANOW, &t_new);
+            fgets(buffer, BUF, stdin);
+            tcsetattr(STDIN_FILENO, TCSANOW, &t_old);
+            printf("\n");
+            //super secret END
             size = strlen(buffer);
+            buffer[size-1] = '\0';
             send(create_socket, buffer, size, 0);
             //size = recv(create_socket, buffer, BUF - 1, 0);
             //recvErrorMsgHandling(size);
